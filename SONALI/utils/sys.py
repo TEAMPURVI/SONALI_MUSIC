236eacd0638411125f7c23f6c765e7468fcf3d4c
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-08-10 19:29:00 +0530

    ᴀʟᴘʜᴀ

--- SONALI/utils/sys.py
+++ SONALI/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from PURVIMUSIC.misc import _boot_
-from PURVIMUSIC.utils.formatters import get_readable_time
+from SONALI.misc import _boot_
+from SONALI.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
