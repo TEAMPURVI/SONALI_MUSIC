8227fd162397b9a357dba33a3811ddbfd993ef1d
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-08-10 19:22:23 +0530

    ᴀʟᴘʜᴀ

--- SONALI/utils/extraction.py
+++ SONALI/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from PURVIMUSIC import app
+from SONALI import app
 
 
 async def extract_user(m: Message) -> User:
