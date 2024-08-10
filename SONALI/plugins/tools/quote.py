8dd79b7d8a5f4d52231518ceaefb63536514b099
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-08-10 18:50:50 +0530

    ᴀʟᴘʜᴀ

--- SONALI/plugins/tools/quote.py
+++ SONALI/plugins/tools/quote.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from PURVIMUSIC import app
+from SONALI import app
 from httpx import AsyncClient, Timeout
 # -----------------------------------------------------------------
 fetch = AsyncClient(
