56b379ef5d5b497f13cb719b10982735e3b7e831
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-08-10 19:10:29 +0530

    ᴀʟᴘʜᴀ

--- SONALI/plugins/tools/sticker.py
+++ SONALI/plugins/tools/sticker.py
@@ -3,7 +3,7 @@ import httpx
 import os
 from pyrogram import filters
 from config import BOT_USERNAME
-from PURVIMUSIC import app
+from SONALI import app
 from pyrogram import filters
 import pyrogram
 from uuid import uuid4
