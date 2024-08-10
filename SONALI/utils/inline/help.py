ee4cac116aef33ba43277f644bfd44cca31626f8
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-08-11 01:34:47 +0530

    ᴀʟᴘʜᴀ

--- SONALI/utils/inline/help.py
+++ SONALI/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from PURVIMUSIC import app
+from SONALI import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
