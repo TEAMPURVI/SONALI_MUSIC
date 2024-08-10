61dd104cfcb437d8b80b4f9cb491d6f8cc66075c
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-08-11 02:19:16 +0530

    ᴀʟᴘʜᴀ

--- SONALI/plugins/tools/gpdata.py
+++ SONALI/plugins/tools/gpdata.py
@@ -1,10 +1,10 @@
 from pyrogram import enums
 from pyrogram.enums import ChatType
 from pyrogram import filters, Client
-from PURVIMUSIC import app
+from SONALI import app
 from config import OWNER_ID
 from pyrogram.types import Message
-from PURVIMUSIC.utils.purvi_ban import admin_filter
+from SONALI.utils.RAUSHAN_ban import admin_filter
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 
