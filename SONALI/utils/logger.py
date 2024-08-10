75cdd93d4fb7b23444ab010d6271a7209e8d6940
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-08-10 19:24:58 +0530

    ᴀʟᴘʜᴀ

--- SONALI/utils/logger.py
+++ SONALI/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from PURVIMUSIC import app
-from PURVIMUSIC.utils.database import is_on_off
+from SONALI import app
+from SONALI.utils.database import is_on_off
 from config import LOGGER_ID
 
 
