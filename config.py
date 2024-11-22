4a9312bf5505cc5740e5f3a7f906d106923b6ed6
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-11-23 03:37:41 +0530

    ᴀʟᴘʜᴀ

--- config.py
+++ config.py
@@ -87,7 +87,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/eg53db.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/h6u7j4.mp4"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/9cevdg.jpg"
