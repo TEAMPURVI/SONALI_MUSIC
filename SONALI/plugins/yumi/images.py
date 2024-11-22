94b8da807e5b2565a7ab547939d2cc7393afb257
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-11-23 01:37:10 +0530

    ᴀʟᴘʜᴀ

--- SONALI/plugins/yumi/images.py
+++ SONALI/plugins/yumi/images.py
@@ -1,38 +1,53 @@
-import requests
-from requests import get 
+import os
+import shutil
+from re import findall
+from bing_image_downloader import downloader
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaPhoto, Message
 from SONALI import app
-from pyrogram import filters
-from pyrogram.types import InputMediaPhoto
 
-@app.on_message(filters.command(["image"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
-async def pinterest(_, message):
-     chat_id = message.chat.id
+@app.on_message(filters.command(["img", "image"], prefixes=["/", "!"]))
+async def google_img_search(client: Client, message: Message):
+    chat_id = message.chat.id
 
-     try:
-       query= message.text.split(None,1)[1]
-     except:
-         return await message.reply("**ɢɪᴠᴇ ɪᴍᴀɢᴇ ɴᴀᴍᴇ ғᴏʀ sᴇᴀʀᴄʜ 🔍**")
+    try:
+        query = message.text.split(None, 1)[1]
+    except IndexError:
+        return await message.reply("❍ ᴘʀᴏᴠɪᴅᴇ ᴀɴ ɪᴍᴀɢᴇ ǫᴜɪᴇʀʏ ᴛᴏ sᴇᴀʀᴄʜ!")
 
-     images = get(f"https://pinterest-api-one.vercel.app/?q={query}").json()
+    lim = findall(r"lim=\d+", query)
+    try:
+        lim = int(lim[0].replace("lim=", ""))
+        query = query.replace(f"lim={lim}", "")
+    except IndexError:
+        lim = 6  # Default limit to 6 images
 
-     media_group = []
-     count = 0
+    download_dir = "downloads"
 
-     msg = await message.reply(f"sᴄʀᴀᴘɪɴɢ ɪᴍᴀɢᴇs ғʀᴏᴍ ᴘɪɴᴛᴇʀᴇᴛs...")
-     for url in images["images"][:6]:
-                  
-          media_group.append(InputMediaPhoto(media=url))
-          count += 1
-          await msg.edit(f"=> ᴏᴡᴏ sᴄʀᴀᴘᴇᴅ ɪᴍᴀɢᴇs {count}")
+    try:
+        downloader.download(query, limit=lim, output_dir=download_dir, adult_filter_off=True, force_replace=False, timeout=60)
+        images_dir = os.path.join(download_dir, query)
+        if not os.listdir(images_dir):
+            raise Exception("No images were downloaded.")
+        lst = [os.path.join(images_dir, img) for img in os.listdir(images_dir)][:lim]  # Ensure we only take the number of images specified by lim
+    except Exception as e:
+        return await message.reply(f"❍ ᴇʀʀᴏʀ ɪɴ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ɪᴍᴀɢᴇs: {e}")
 
-     try:
-        
+    msg = await message.reply("❍ sᴏɴᴀʟɪ ғɪɴᴅɪɴɢ ɪᴍᴀɢᴇs.....")
+
+    count = 0
+    for img in lst:
+        count += 1
+        await msg.edit(f"❍ sᴏɴᴀʟɪ ғɪɴᴅ {count} ɪᴍᴀɢᴇs.....")
+
+    try:
         await app.send_media_group(
-                chat_id=chat_id, 
-                media=media_group,
-                reply_to_message_id=message.id)
-        return await msg.delete()
-
-     except Exception as e:
-           await msg.delete()
-           return await message.reply(f"ᴇʀʀᴏʀ : {e}")
+            chat_id=chat_id,
+            media=[InputMediaPhoto(media=img) for img in lst],
+            reply_to_message_id=message.id
+        )
+        shutil.rmtree(images_dir)
+        await msg.delete()
+    except Exception as e:
+        await msg.delete()
+        return await message.reply(f"❍ ᴇʀʀᴏʀ ɪɴ sᴇɴᴅɪɴɢ ɪᴍᴀɢᴇs: {e}")
