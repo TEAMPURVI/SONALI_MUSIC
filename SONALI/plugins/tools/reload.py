d90af05831b4ab5279ba959769c9d2b7df2df66f
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-11-01 08:14:44 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- SONALI/plugins/tools/reload.py
+++ SONALI/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7302887101)
+    & filters.user(7473021518)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
